8946d384f35cdec5bc5365d5ea7a5d26a6b740ea
vladislavsumin <vladislavsumin@mail.ru>
2016-12-17 16:54:38 +0300

    Add log4j2, create VFrame main class

---
+++ src/main/java/ru/vladislavsumin/vframe/VFrame.java
@@ -0,0 +1,30 @@
+package ru.vladislavsumin.vframe;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Main library class.
+ * <p>
+ * Must be init before use another library classes.
+ *
+ * @author Sumin Vladislav
+ * @version 0.1
+ */
+public class VFrame {
+    private static final Logger log = LogManager.getLogger();
+
+    /**
+     * Initialize VFrame library.
+     */
+    public static void init() {
+
+    }
+
+    /**
+     * Stop all internal library threads.
+     */
+    public static void stop() {
+
+    }
+}
---
+++ src/test/java/ru/vladislavsumin/vframetest/Main.java
@@ -0,0 +1,14 @@
+package ru.vladislavsumin.vframetest;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger log = LogManager.getLogger();
+
+
+    public static void main(String[] args) {
+        log.trace("Test running");
+    }
+}
