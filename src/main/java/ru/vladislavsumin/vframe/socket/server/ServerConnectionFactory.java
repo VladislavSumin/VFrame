6128be69417365bd472b67064aeca27950055e90
vladislavsumin <vladislavsumin@mail.ru>
2017-02-05 18:40:51 +0300

    Удаление старого исходного кода

--- src/main/java/ru/vladislavsumin/vframe/socket/server/ServerConnectionFactory.java
+++
@@ -1,8 +0,0 @@
-package ru.vladislavsumin.vframe.socket.server;
-
-
-import java.net.Socket;
-
-public interface ServerConnectionFactory {
-    ServerConnectionAbstract createNewConnection(Socket socket, ServerSocketWorker parent);
-}
