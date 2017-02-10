72cce5ef9714c074260e7f8ca5e92fd5628bdf0e
VladislavSumin <vladislavsumin@mail.ru>
2017-02-11 00:08:16 +0300

    work with subscription manager part 1

--- server/src/main/java/ru/falseteam/vframe/socket/SubscriptionManager.java
+++ server/src/main/java/ru/falseteam/vframe/socket/SubscriptionManager.java
@@ -13,35 +13,49 @@ import java.util.Map;
  * Subscription manager.
  *
  * @author Sumin Vladislav
- * @version 1.1
+ * @version 2.0
  */
 public class SubscriptionManager<T extends Enum<T>> {
     public interface SubscriptionInterface {
         Map<String, Object> getAllData();
     }
 
-    private final Map<String, Pair<SubscriptionInterface, List<ConnectionAbstract>>> events = new HashMap<>();
+    private class Data {
+        final SubscriptionInterface subscriptionInterface;
+        final List<ConnectionAbstract<T>> subscribers;
+        final T[] permissions;
 
-    public void addEvent(String name, SubscriptionInterface allInfoMethod) {
+        Data(SubscriptionInterface subscriptionInterface, T[] permissions) {
+            this.subscriptionInterface = subscriptionInterface;
+            this.permissions = permissions;
+            subscribers = new LinkedList<>();
+        }
+    }
+
+    private final Map<String, Data> events = new HashMap<>();
+
+    public void addEvent(String name, SubscriptionInterface allInfoMethod, T... permissions) {
         synchronized (events) {
-            events.put(name, new Pair<>(allInfoMethod, new LinkedList<>()));
+            events.put(name, new Data(allInfoMethod, permissions));
         }
     }
 
     private Map<String, Object> getAllData(String eventName) {
-        return events.get(eventName).getKey().getAllData();
+        return events.get(eventName).subscriptionInterface.getAllData();
     }
 
     public void onEventDataChange(String eventName, Map<String, Object> newData) {
-        events.get(eventName).getValue().forEach(connection ->
-                connection.send(dataUpdate(eventName, getAllData(eventName))));
-        //TODO отправлять только изменения а не все по новой
+        synchronized (events) {
+            events.get(eventName).subscribers.forEach(connection ->
+                    connection.send(dataUpdate(eventName, newData)));
+            //TODO отправлять только изменения а не все по новой
+        }
     }
 
-    public boolean addSubscription(String eventName, ConnectionAbstract connection) {
+    public boolean addSubscription(String eventName, ConnectionAbstract<T> connection) {
         synchronized (events) {
             if (events.containsKey(eventName)) {
-                events.get(eventName).getValue().add(connection);
+                events.get(eventName).subscribers.add(connection);
                 connection.send(dataUpdate(eventName, getAllData(eventName)));
                 return true;
             }
@@ -49,19 +63,19 @@ public class SubscriptionManager<T extends Enum<T>> {
         }
     }
 
-    public boolean removeSubscription(String eventName, ConnectionAbstract connection) {
+    public boolean removeSubscription(String eventName, ConnectionAbstract<T> connection) {
         synchronized (events) {
             if (events.containsKey(eventName)) {
-                events.get(eventName).getValue().remove(connection);
+                events.get(eventName).subscribers.remove(connection);
                 return true;
             }
             return false;
         }
     }
 
-    public void removeSubscriber(ConnectionAbstract connection) {
+    public void removeSubscriber(ConnectionAbstract<T> connection) {
         synchronized (events) {
-            events.forEach((s, pair) -> pair.getValue().remove(connection));
+            events.forEach((s, pair) -> pair.subscribers.remove(connection));
         }
     }
 
