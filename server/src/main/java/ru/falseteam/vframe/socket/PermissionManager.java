6ecd2abcbab9a7f50c8fc6e1b27703b0905d0b8f
VladislavSumin <vladislavsumin@mail.ru>
2017-02-10 22:38:12 +0300

    permission manager, добавил описание, убрал лишнее

--- server/src/main/java/ru/falseteam/vframe/socket/PermissionManager.java
+++ server/src/main/java/ru/falseteam/vframe/socket/PermissionManager.java
@@ -1,12 +1,24 @@
 package ru.falseteam.vframe.socket;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * access level manager
+ *
+ * @param <T> - enum with permissions
+ * @author Sumin Vladislav
+ * @version 1.0
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class PermissionManager<T extends Enum<T>> {
+    public interface DefaultProtocol {
+        void exec(Container container, ConnectionAbstract connection);
+    }
+
     private final Map<T, Map<String, ProtocolAbstract>> permissions = new HashMap<>();
     private final T defaultPermission;
+    private DefaultProtocol defaultProtocol = null;
 
     public PermissionManager(Class<T> permissionEnum, T defaultPermission) {
         this.defaultPermission = defaultPermission;
@@ -15,26 +27,19 @@ public class PermissionManager<T extends Enum<T>> {
         }
     }
 
-    public void addCommand(ProtocolAbstract c, T... groupies) {
+    @SafeVarargs
+    public final void addCommand(ProtocolAbstract c, T... groupies) {
         for (T g : groupies) permissions.get(g).put(c.getName(), c);
-    }
-
-
-    public interface DefaultProtocol {
-        void exec(Container container, ConnectionAbstract connection);
-    }
-
-    private DefaultProtocol defaultProtocol = null;
-
-    DefaultProtocol getDefaultProtocol() {
-        return defaultProtocol;
     }
 
     public void setDefaultProtocol(DefaultProtocol defaultProtocol) {
         this.defaultProtocol = defaultProtocol;
     }
 
-    @SuppressWarnings("SuspiciousMethodCalls")
+    DefaultProtocol getDefaultProtocol() {
+        return defaultProtocol;
+    }
+
     Map<String, ProtocolAbstract> getProtocols(T g) {
         return permissions.get(g);
     }
