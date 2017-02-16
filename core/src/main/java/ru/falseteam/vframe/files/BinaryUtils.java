27448a9a640637d28a9d5baffa739e6da13306bd
VladislavSumin <vladislavsumin@mail.ru>
2017-02-16 07:59:09 +0300

    binary utils part 2

--- core/src/main/java/ru/falseteam/vframe/files/BinaryUtils.java
+++ core/src/main/java/ru/falseteam/vframe/files/BinaryUtils.java
@@ -1,7 +1,54 @@
 package ru.falseteam.vframe.files;
 
+import ru.falseteam.vframe.VFrameRuntimeException;
+
+import java.io.*;
+
 /**
- * Created by Vlad on 16.02.2017.
+ * Set of utilities from work with binary files.
+ * Набор утилит для работы с бинарными файлами.
+ *
+ * @author Sumin Vladislav
  */
 public class BinaryUtils {
+    /**
+     * Load binary file and cast him to type T.
+     *
+     * @param path path to file.
+     * @param <T>  binary file cast to T
+     * @return {T} if successfully read binary file of {null} if not.
+     */
+    @SuppressWarnings("unchecked")
+    static <T> T loadFromBinaryFile(String path) {
+        try {
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(path));
+            T data = (T) stream.readObject();
+            stream.close();
+            return data;
+        } catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+
+    /**
+     * Save binary file to local storage
+     * 
+     * @param data data to save
+     * @param path path to save
+     * @param <T>  type of data
+     * @throws {@link RuntimeException} if can not save file.
+     */
+    @SuppressWarnings("JavaDoc")
+    static <T> void saveToBinaryFile(T data, String path) {
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(path));
+            stream.writeObject(data);
+            stream.flush();
+            stream.close();
+        } catch (IOException e) {
+            throw new VFrameRuntimeException(e);
+        }
+        //TODO а нужен ли здесь тип Т или можно object сохранять?
+    }
 }
